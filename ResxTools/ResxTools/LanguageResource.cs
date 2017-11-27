0f1d9712fe8ae57013b331b8cbe0f4278709a8e2
Jonas Pegerfalk <jonas.pegerfalk@gmail.com>
2017-11-27 08:36:26 +0100

    Parse resource files and output all resource keys.

---
+++ ResxTools/ResxTools/LanguageResource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+using System.Collections;
+
+namespace ResxTools
+{
+    class LanguageResource
+    {
+        private ResXResourceReader resXResourceReader;
+
+        public LanguageResource(ResXResourceReader resXResourceReader)
+        {
+            this.resXResourceReader = resXResourceReader;
+        }
+
+        public IEnumerable<string> ResourceNames
+        {
+            get
+            {
+                IEnumerable<DictionaryEntry> enumerator = resXResourceReader.OfType<DictionaryEntry>();
+                return enumerator.Select(o => o.Key as string);
+            }
+        }
+
+        public static LanguageResource ReadFromResourceFile(string fileName)
+        {
+            ResXResourceReader resXResourceReader = new ResXResourceReader(fileName) { UseResXDataNodes = true };
+            return new LanguageResource(resXResourceReader);
+        }
+    }
+}
--- ResxTools/ResxTools/Program.cs
+++ ResxTools/ResxTools/Program.cs
@@ -10,6 +10,20 @@ namespace ResxTools
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Usage:");
+                Console.WriteLine($" {System.AppDomain.CurrentDomain.FriendlyName} resorucefilename");
+                return;
+            }
+            string filename = args[0];
+            var resources = LanguageResource.ReadFromResourceFile(filename);
+
+            resources.ResourceNames.ToList().ForEach(r => Console.WriteLine(r));
+
+
+            Console.ReadKey();
+
         }
     }
 }
