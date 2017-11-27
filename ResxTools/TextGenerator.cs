15a651f914b8687e28d38c34c6d905fbf64508ae
Jonas Pegerfalk <jonas.pegerfalk@gmail.com>
2017-11-27 12:34:14 +0100

    Added text generator.

---
+++ ResxTools/TextGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxTools
+{
+    public class TextGenerator
+    {
+        private static Random random = new Random();
+     
+        public static string Generate(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz            ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ Tests/TextGeneratorUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using ResxTools;
+using System.IO;
+using System.Linq;
+
+namespace UnitTests
+{
+    public class TextGeneratorUnitTests
+    {
+        [Fact]
+        public void GenerateText_ShouldBeCorrectLength()
+        {
+            string t = TextGenerator.Generate(20);
+            Assert.Equal(20, t.Length);
+        }
+
+
+    }
+}
