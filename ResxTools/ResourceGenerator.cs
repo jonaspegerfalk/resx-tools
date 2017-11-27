b8f5dcd274202bdedff4084bb03cbb4657ad1a09
Jonas Pegerfalk <jonas.pegerfalk@gmail.com>
2017-11-27 12:59:56 +0100

    Added ResourceGenerator that writes a resource file to a string.

---
+++ ResxTools/ResourceGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxTools
+{
+    public class ResourceGenerator
+    {
+        private IEnumerable<ResourceData> data;
+
+        public ResourceGenerator(IEnumerable<ResourceData> data)
+        {
+            this.data = data;
+        }
+        public static ResourceGenerator CreateGenerator(IEnumerable<ResourceData> data)
+        {
+            return new ResourceGenerator(data);
+        }
+
+        public string WriteToString()
+        {
+            MemoryStream stream = new MemoryStream();
+            ResXResourceWriter writer = new ResXResourceWriter(stream);
+            data.ToList().ForEach(d=>writer.AddResource(new ResXDataNode(d.Key, d.Value) { Comment = d.Comment }));
+            writer.Generate();
+            stream.Position = 0;
+            var streamReader = new StreamReader(stream);
+            return streamReader.ReadToEnd() ;
+        }
+    }
+}
---
+++ Tests/ResourceGeneratorUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using ResxTools;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    public class ResourceGeneratorUnitTests
+    {
+        [Fact]
+        public void ResourceGenerator_ShouldGenerateResourceFile()
+        {
+            var resources = new List<ResourceData>() {
+                new ResourceData() { Key = "KeyString1", Value = "Value1", Comment = "MaxLength=20" },
+                new ResourceData() { Key = "KeyString2", Value = "Value2", Comment = "MaxLength=20" }};
+
+            var generator = ResourceGenerator.CreateGenerator(resources);
+            var generatedResourceData = generator.WriteToString();
+
+            Assert.NotEmpty(generatedResourceData);
+            Assert.Contains("KeyString1", generatedResourceData);
+            Assert.Contains("MaxLength", generatedResourceData);
+        }
+
+
+    }
+}
